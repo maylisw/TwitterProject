6872fdd4771563bc316f7cf4f833802d6950e79d
maylisw <maylisw31@gmail.com>
2017-05-10 13:37:15 -0700

    comments

--- src/TweetTweet.java
+++ src/TweetTweet.java
@@ -6,7 +6,7 @@ public class TweetTweet {
         System.out.printf("lame");
         // Create URL object
         // URL.getText - > plain text
-        //
-        
+        //plain test.split(" ")
+
     }
 }
