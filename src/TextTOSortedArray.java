c4dce180dce8d37d159f347620bc844c23d7b885
maylisw <maylisw31@gmail.com>
2017-05-10 22:07:18 -0700

    Maylis #4 Finished Plain text to sorted array Not tested
    Signed-off-by: maylisw <maylisw31@gmail.com>

--- src/TextTOSortedArray.java
+++ src/TextTOSortedArray.java
@@ -24,5 +24,6 @@ public class TextTOSortedArray {
                 s[i] = fin; //set new edited string to index i
             }
         }
+        return s;
     }
 }
--- src/TweetTweet.java
+++ src/TweetTweet.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by mayliswhetsel on 5/8/17.
  * Main class: 2D Array --> Tweet --> Image --> Twitter
@@ -7,11 +9,12 @@ public class TweetTweet {
         System.out.printf("lame");
         // Create URL object
         // URL.getText - > plain text
-        String plainText = "Tester string righ here!"
+        String plainText = "Tester string right here!";
         TextTOSortedArray obj = new TextTOSortedArray(plainText);
-        tokenArray = obj.tokenize();
-        betterTokenArray = obj.sort(tokenArray);
+        String[] tokenArray = obj.tokenize();
+        String[] betterTokenArray = obj.sort(tokenArray);
+        Arrays.sort(betterTokenArray);
+        System.out.println(betterTokenArray.toString());
 
-        //
     }
 }
