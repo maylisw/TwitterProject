f10132303186e8f54e1962e63f30a7b60cba1ca5
CarolineXWL89 <carolinexwliu89@hotmail.com>
2017-05-12 13:39:30 -0700

    Caroline7
    Signed-off-by: CarolineXWL89 <carolinexwliu89@hotmail.com>

--- src/PartOfSpeech.java
+++ src/PartOfSpeech.java
@@ -1,4 +1,11 @@
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by princ on 10/05/2017.
@@ -6,11 +13,26 @@ import java.util.ArrayList;
  */
 public class PartOfSpeech {
     //instance variables to be determined later
-    ArrayList<Word> words = new ArrayList<>();
+    private  ArrayList<Word> words = new ArrayList<>();
 
 
     public PartOfSpeech(ArrayList<Word> words){
         this.words = words;
-    } //should work
+    }
 
+    public String tagging(ArrayList<Word> wordObj){
+        MaxentTagger tagger = new MaxentTagger("english-left3words-distsim.tagger");//model fie from their models
+        //List<List<HasWord>> sentences = MaxentTagger.tokenizeText(new BufferedReader(new StringReader(paragraph)));
+        int l = wordObj.size();
+        ArrayList<>
+        for(int i = 0; i < l; i++){
+            for (List<HasWord> sentence : sentences) {
+                List<TaggedWord> tSentence = tagger.tagSentence(sentence);
+                for(TaggedWord t: tSentence )
+                {
+                    h.put((String) t, (string) t.tag());
+                }
+            }
+        }
+    }
 }
