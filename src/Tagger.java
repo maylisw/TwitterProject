5c99c1f9e94ed5ab2b7bc72999cabf7973a21003
maylisw <maylisw31@gmail.com>
2017-05-12 14:49:11 -0700

    Maylis #16 complete tagger
    Signed-off-by: maylisw <maylisw31@gmail.com>

--- src/Tagger.java
+++ src/Tagger.java
@@ -5,92 +5,82 @@ import java.util.ArrayList;
  */
 public class Tagger {
     private ArrayList<Word> tagME;
-    public Tagger(ArrayList<Word> w){
+
+    public Tagger(ArrayList<Word> w) {
         tagME = w;
     }
-    public POSArrayLists listTag(){
-       for(Word w: tagME){
-           ArrayList<Word> NN = new ArrayList<>();
-           ArrayList<Word> NNS = new ArrayList<>();
-           ArrayList<Word> VB = new ArrayList<>();
-           ArrayList<Word> VBP = new ArrayList<>();
-           ArrayList<Word> VBZ = new ArrayList<>();
-           ArrayList<Word> NNP = new ArrayList<>();
-           ArrayList<Word> NNPS = new ArrayList<>();
-           ArrayList<Word> JJ = new ArrayList<>();
-           ArrayList<Word> JJR = new ArrayList<>();
-           ArrayList<Word> JJS = new ArrayList<>();
-           ArrayList<Word> VBD = new ArrayList<>();
-           ArrayList<Word> VBN = new ArrayList<>();
-           PartOfSpeech p = new PartOfSpeech(w);
-           String partOfSpeech = p.tagging();
-           switch(partOfSpeech){
-               case "NN":
-               {
-                   NN.add(w);
-                   break;
-               }
-               case "NNS":
-               {
-                   NNS.add(w);
-                   break;
-               }
-               case "VB":
-               {
-                   VB.add(w);
-                   break;
-               }
-               case "VBP":
-               {
-                   VBP.add(w);
-                   break;
-               }
-               case "VBZ":
-               {
-                   VBZ.add(w);
-                   break;
-               }
-               case "NNP":
-               {
-                   NNP.add(w);
-                   break;
-               }
-               case "NNPS":
-               {
-                   NNPS.add(w);
-                   break;
-               }
-               case "JJ":
-               {
-                   JJ.add(w);
-                   break;
-               }
-               case "JJR":
-               {
-                   JJR.add(w);
-                   break;
-               }
-               case "JJS":
-               {
-                   JJS.add(w);
-                   break;
-               }
-               case "VBD":
-               {
-                   VBD.add(w);
-                   break;
-               }
-               case "VBN":
-               {
-                   VBN.add(w);
-                   break;
-               }
-               default:
-               {
-                   break;
-               }
-           }
 
-       }
+    public POSArrayLists listTag() {
+        ArrayList<Word> NN = new ArrayList<>();
+        ArrayList<Word> NNS = new ArrayList<>();
+        ArrayList<Word> VB = new ArrayList<>();
+        ArrayList<Word> VBP = new ArrayList<>();
+        ArrayList<Word> VBZ = new ArrayList<>();
+        ArrayList<Word> NNP = new ArrayList<>();
+        ArrayList<Word> NNPS = new ArrayList<>();
+        ArrayList<Word> JJ = new ArrayList<>();
+        ArrayList<Word> JJR = new ArrayList<>();
+        ArrayList<Word> JJS = new ArrayList<>();
+        ArrayList<Word> VBD = new ArrayList<>();
+        ArrayList<Word> VBN = new ArrayList<>();
+        for (Word w : tagME) {
+            PartOfSpeech p = new PartOfSpeech(w);
+            String partOfSpeech = p.tagging();
+            switch (partOfSpeech) {
+                case "NN": {
+                    NN.add(w);
+                    break;
+                }
+                case "NNS": {
+                    NNS.add(w);
+                    break;
+                }
+                case "VB": {
+                    VB.add(w);
+                    break;
+                }
+                case "VBP": {
+                    VBP.add(w);
+                    break;
+                }
+                case "VBZ": {
+                    VBZ.add(w);
+                    break;
+                }
+                case "NNP": {
+                    NNP.add(w);
+                    break;
+                }
+                case "NNPS": {
+                    NNPS.add(w);
+                    break;
+                }
+                case "JJ": {
+                    JJ.add(w);
+                    break;
+                }
+                case "JJR": {
+                    JJR.add(w);
+                    break;
+                }
+                case "JJS": {
+                    JJS.add(w);
+                    break;
+                }
+                case "VBD": {
+                    VBD.add(w);
+                    break;
+                }
+                case "VBN": {
+                    VBN.add(w);
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+        return new POSArrayLists(NN, NNS, VB, VBP, VBZ, NNP, NNPS, JJ, JJR, JJS, VBD, VBN);
     }
 }
+
