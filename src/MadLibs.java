d90adeac59ffedc8d3c649311fd8707d789fcf7f
maylisw <maylisw31@gmail.com>
2017-05-18 12:36:07 -0700

    Maylis #25 half of madlibs done
    Signed-off-by: maylisw <maylisw31@gmail.com>

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,12 +1,59 @@
+import java.util.ArrayList;
+
 /**
  * Created by mayliswhetsel on 5/16/17.
  */
-public class MadLibs {
-    /*
-    instance variables
+/*public class MadLibs {
+    private String tweet;
+    /*private int numberOfSingularNouns; 0
+    private int numberOfPluralNouns; 1
+    private int numberOfInfiniteVerbs; 2
+    private int numberOfNonThirdPersonSingularPresentVerbs; 3
+    private int numberOfThirdPersonSingularVerbs; 4
+    private int numberOfProperNounSingulars; 5
+    private int numberOfProperNounPlurals; 6
+    private int numberOfAdjectives;7
+    private int numberOfComparativeAdjectives; 8
+    private int numberOfSuperlativeAdjectives; 9
+    private int numberOfPastTenseVerbs; 10
+    private int numberOfPastParticipleVerbs; 11
+    */
+ /*   public MadLibs(double n, String[][] words)
+    {
+        int k = (int) n*6 +1;
+        switch(k)
+        {
+            case 1:
+                tweet = "The real problem is that " + words[1][0] + " " + words[4][0] + "! Very un-American!";
+                break;
+            case 2:
+                tweet = "Despite the efforts of " + words[8][0] + " " + words[5][0] + " to stop us, " + words[0][0] + " is moving quickly!";
+                break;
+            case 3:
+                tweet = words[5][0] + " just stated that (noun) (past tense verb). Very untrue!";
+                break;
+            case 4:
+                tweet = "";
+                break;
+            case 5:
+                tweet = "";
+                break;
+            case 6:
+                tweet = "";
+                break;
+        }
 
-    constructor --> takes in the 2D array and double for the template
+    }
 
-    public String plugIn(String[][] s, double d){}
-     */
+    public String getTweet() {
+        return tweet;
+    }
 }
+         \n" +
+        "\n" +
+        "Reports that (noun) (past tense verb) by @CNN are completely untrue- FAKE NEWS!\n" +
+        "\n" +
+        "Nobody should be allowed to (verb) the (noun)! Wrong wrong wrong!\n" +
+        "\n" +
+        "Finally a news outlet that is reporting the truth, (noun) is (adjective). Very True!\n"
+        */
