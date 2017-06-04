b9a65011cf8ee3b080739d7c8f2a48f5cee58521
maylisw <maylisw31@gmail.com>
2017-06-03 18:24:34 -0700

    Maylis #41 Got tweet to work but i'm leaving it commented out so that we don't keep tweeting
    Signed-off-by: maylisw <maylisw31@gmail.com>

--- src/TweetTweet.java
+++ src/TweetTweet.java
@@ -3,30 +3,40 @@ import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
 import javax.swing.*;
 import java.util.Arrays;
 import java.util.ArrayList;
+import twitter4j.conf.ConfigurationBuilder;
+import twitter4j.TwitterFactory;
+import twitter4j.Twitter;
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 /**
  * Created by mayliswhetsel on 5/8/17.
  * Main class: 2D Array --> Tweet --> Image --> Twitter
  */
 public class TweetTweet {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         UserInput userInput = new UserInput();
         System.out.print("What article do you want to make Trump react to? --> ");
         String link = userInput.getString();
         String realURL = "http://boilerpipe-web.appspot.com/extract?url=" + link + "&output=text";
         System.out.println(realURL);
         //String boilerPipeURL = "http://boilerpipe-web.appspot.com/extract?url=https://www.theodysseyonline.com/rape-culture-important-details-missed&output=text";
+
         URLReader url = new URLReader(realURL); //sample text for now
         String boilerPipe = url.readerReturn(realURL);
+
         //HTMLtoPlainText html = new HTMLtoPlainText();
         //String plainText = html.replace(HTMLCode);
+
         TextTOSortedArray obj = new TextTOSortedArray(boilerPipe);
         String[] tokenArray = obj.tokenize();
+
         String[] tokenArray2 = obj.punctuationRemoval(tokenArray);
         String[] tokenArray3 = obj.punctuationRemoval(tokenArray2);
         String[] tokenArray4 = obj.punctuationRemoval(tokenArray3);
         String[] betterTokenArray = obj.frontPunctuationRemoval(tokenArray4);
         Arrays.sort(betterTokenArray);
+
         SortedTOWordObjectArrayList obj2 = new SortedTOWordObjectArrayList(betterTokenArray);
         ArrayList<Word> wordList = obj2.listed();
         PartOfSpeech p = new PartOfSpeech(wordList, boilerPipe);
@@ -35,8 +45,10 @@ public class TweetTweet {
         boolean goodBad = posNeg.whichTemplate();
 
         POSArrayLists taggedLists = p.tagging();
+
         Chosen c = new Chosen(taggedLists, goodBad);
         String [][] filled = c.fill(taggedLists);
+
         double d = c.getTemplate();
         MadLibs madLibs = new MadLibs(d, filled);
         String tweet = madLibs.getTweet();
@@ -45,16 +57,18 @@ public class TweetTweet {
         /*String[][] s = {{"hi", "me", "what even"}, {"boom", "boo", "hello"}, {"When", "the", "home"}, {"And", "window", "close"}, {"their", "shutters", "One"}, {"by", "one", "oil"}, {"lamps", "are", "blown"}, {"out", "Until", "dark"}, {"envelopes", "single", "flame"}, {"Pulsing", "softly", "hearth"}, {"wandering", "soul", "glimmers"}, {"base", "stairs", "nighttime"}};
         MadLibs madLibs = new MadLibs(0.6, s); //tester MadLibs object
         String tweet = madLibs.getTweet(); //why are only 1 and 6 working? --> k < 0 = 1; k > 0 = 6*/
-        System.out.print(tweet);
-        /*Icon image = new ImageIcon("TrumpTweetPage.png");
-        TweetImage tweetImage = new TweetImage(tweet, image, JLabel.CENTER);
-        JFrame jFrame = tweetImage.makeJLabel();*/
 
-        /*System.out.print(jFrame);
-        int w = image.getIconWidth();
-        int h = image.getIconHeight();
-        JLabel imageTweet = new JLabel(tweet, image, JLabel.CENTER);
-        imageTweet.setIcon(image);
-        System.out.print(imageTweet); //printing out info at JLabel imageTweet*/
+        /*
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("Kcv2jop3cdiHPFAM1Gik8hirB")
+                .setOAuthConsumerSecret("dz2EVFTJWPvBnZwszoOI4AHifOk5izx9ZMWsiV61LRAUt5qhql")
+                .setOAuthAccessToken("870735718355574784-QtJCCRPArbSuzZPDpaS5ilaBQ8ZXkY4")
+                .setOAuthAccessTokenSecret("eremAiNxiuAvmFQhIshTXoT47g4VcWt6xx1qvQCdKH5Io");
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        Twitter twitter = tf.getInstance();
+        Status status = twitter.updateStatus(tweet);
+        System.out.println("Successfully updated the status to [" + status.getText() + "].");
+        */
     }
 }
