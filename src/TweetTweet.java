70f8e1c389ba8fa3310b0662d79ae602c2a42d34
maylisw <Maylisw31@gmail.com>
2019-09-23 18:06:54 -0400

    fixed some stuff + added links

--- src/TweetTweet.java
+++ src/TweetTweet.java
@@ -52,7 +52,7 @@ public class TweetTweet {
         PositiveNegative posNeg = new PositiveNegative(realURL);
         boolean goodBad = posNeg.whichTemplate();
         String s = posNeg.whichNews();
-        //System.out.println(goodBad);
+        //System.out.println(goodBad)
 
         POSArrayLists taggedLists = p.tagging();
 
